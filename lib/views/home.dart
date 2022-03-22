7ead4e8e0278ceb27ebc923e3702d87fdcabaffe
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-03-22 19:59:05 +0545

    Created Recipe Card

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipie/views/widgets/recipe_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -29,6 +30,12 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+      body: const RecipeCard(
+          title: "My Recipe",
+          rating: '3',
+          cookTime: "20 Mins",
+          thumbnailUrl:
+              'https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
     );
   }
 }
