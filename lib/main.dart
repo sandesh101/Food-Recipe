42a0169ecf923ed6071304a0f764968917f8fc02
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-03-22 19:10:53 +0545

    Added http Package

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,14 @@ class FoodApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          primaryColor: Colors.white,
-          textTheme: const TextTheme(
-            bodyText2: TextStyle(color: Colors.white),
-          )),
-      home: const Scaffold(),
-    );
+        title: 'Food App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: Colors.white,
+            textTheme: const TextTheme(
+              bodyText2: TextStyle(color: Colors.white),
+            )),
+        home: const Scaffold());
   }
 }
